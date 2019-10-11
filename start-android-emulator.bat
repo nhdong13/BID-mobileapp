393e6e6bb22764b3d681df6916c9e04325a94596
phucphung <phungthienphuc18@gmail.com>
2019-10-11 12:46:35 +0700

    create start emulator file for android

---
+++ start-android-emulator.bat
@@ -0,0 +1,21 @@
+@echo off
+
+if exist %ANDROID_HOME% goto :EVNotFound
+if exist %ANDROID_HOME% goto :StartAndroid
+
+:EVNotFound
+echo You haven't set the ANDROID_HOME Environment Variable
+echo To set your ANDROID_HOME PATH search "Edit environtment variables for your account" or "Edit system variables"
+echo Go to Android Studio -^> 'SDK Manager' 
+echo or 'Settings -^> 'Appearance ^& Behavior' -^> 'System Settings' -^> 'Android SDK'
+
+exit /b 0
+
+:StartAndroid
+echo Initiating android devices...
+
+cd %ANDROID_HOME%
+
+emulator -avd Nexus_5X_API_28
+
+pause
