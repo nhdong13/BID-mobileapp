c208515d8a8728364e1cf4e94e13549e12d51687
dong-0925 <hoang.nhat.dong@sun-asterisk.com>
2019-11-04 14:09:54 +0700

    feedback module

---
+++ src/screens/parent/RequestDetail.js
@@ -0,0 +1,374 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  ScrollView,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons/';
+import { MuliText } from 'components/StyledText';
+import moment from 'moment';
+import Api from 'api/api_helper';
+import { withNavigation } from 'react-navigation';
+import StarRating from 'react-native-star-rating';
+
+export class SittingDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sittingRequestsID: this.props.navigation.state.params.requestId,
+      date: null,
+      startTime: null,
+      endTime: null,
+      address: null,
+      price: 0,
+      bsitter: null,
+      isModalVisible: false,
+      starCount: 0,
+      isRated: false,
+    };
+    this.callDetail = this.callDetail.bind(this);
+  }
+
+  componentDidMount() {
+    Api.get('sittingRequests/' + this.state.sittingRequestsID.toString()).then(
+      (resp) => {
+        this.setState({
+          date: resp.sittingDate,
+          startTime: resp.startTime,
+          endTime: resp.endTime,
+          address: resp.sittingAddress,
+          bsitter: resp.bsitter,
+          price: resp.totalPrice,
+        });
+      },
+    );
+    Api.get('feedback/' + this.state.sittingRequestsID.toString()).then(res=> {
+      if (res != null) {
+        this.setState({ starCount: res[0].rating, isRated: true, });
+      }
+    });
+  }
+
+  onStarRatingPress(rating) {
+    const body = {
+      rating: rating,
+      requestId: this.state.sittingRequestsID,
+    };
+    if (!this.state.isRated) {
+      Api.post('feedback/', body);
+      this.setState({ starCount: rating });
+    }
+  }
+
+  callDetail() {
+    if (this.state.isModalVisible) {
+      this.setState({ isModalVisible: false });
+    } else {
+      this.setState({ isModalVisible: true });
+    }
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <View style={{ marginHorizontal: 30, backgroundColor: 'white' }}>
+          <View style={styles.detailInformationContainer}>
+            <View style={styles.informationText}>
+              <Ionicons
+                name="ios-calendar"
+                size={17}
+                style={{ marginBottom: -5 }}
+                color="#bdc3c7"
+              />
+              <MuliText style={styles.contentInformationDate}>
+                {moment(this.state.date).format('dddd Do MMMM')}
+              </MuliText>
+            </View>
+            <View style={styles.informationText}>
+              <Ionicons
+                name="ios-cash"
+                size={17}
+                style={{ marginBottom: -5 }}
+                color="#bdc3c7"
+              />
+              <MuliText style={styles.contentInformation}>
+                {this.state.price} VND
+              </MuliText>
+            </View>
+            <View style={styles.informationText}>
+              <Ionicons
+                name="ios-timer"
+                size={17}
+                style={{ marginBottom: -5 }}
+                color="#bdc3c7"
+              />
+              <MuliText style={styles.contentInformation}>
+                {moment.utc(this.state.startTime, 'HH:mm').format('HH:mm')} -
+                {moment.utc(this.state.endTime, 'HH:mm').format('HH:mm')}
+              </MuliText>
+            </View>
+            <View style={styles.informationText}>
+              <Ionicons
+                name="ios-home"
+                size={17}
+                style={{ marginBottom: -5 }}
+                color="#bdc3c7"
+              />
+              <MuliText style={styles.contentInformation}>
+                {this.state.address}
+              </MuliText>
+            </View>
+          </View>
+          {/* render babysitter if exist */}
+          {this.state.bsitter ? (
+            <View style={styles.detailPictureContainer}>
+              <Image
+                source={this.state.detailPictureSitter}
+                style={styles.profileImg}
+              />
+              <View style={styles.leftInformation}>
+                <MuliText style={styles.pictureInformation}>
+                  Người giữ trẻ
+                </MuliText>
+                <MuliText style={{ fontSize: 15 }}>
+                  {this.state.bsitter.nickname}
+                </MuliText>
+              </View>
+            </View>
+          ) : (
+            <View />
+          )}
+          {/* end */}
+          <StarRating
+            starStyle={{
+              marginLeft: 10,
+            }}
+            fullStarColor="#f6ff00"
+            starSize={30}
+            disabled={false}
+            maxStars={5}
+            rating={this.state.starCount}
+            selectedStar={(rating) => this.onStarRatingPress(rating)}
+          />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+export default withNavigation(SittingDetail);
+
+SittingDetail.navigationOptions = {
+  title: 'Yêu cầu chi tiết',
+};
+
+const styles = StyleSheet.create({
+  rightInformationSitter: {
+    marginLeft: 'auto',
+  },
+  rightInformation: {
+    marginLeft: 'auto',
+    marginTop: 10,
+  },
+  pictureInformationSitter: {
+    fontSize: 13,
+    fontWeight: '400',
+    color: '#bdc3c7',
+  },
+  pictureInformation: {
+    fontSize: 13,
+    fontWeight: '400',
+    color: '#bdc3c7',
+  },
+  leftInformationSitter: {
+    marginLeft: 5,
+  },
+  leftInformation: {
+    marginTop: 5,
+    marginLeft: 5,
+  },
+  childrenInformationContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    marginHorizontal: 10,
+    marginTop: 10,
+    marginBottom: 5,
+    borderRadius: 10,
+    height: 80,
+    width: 140,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.8,
+    shadowRadius: 4,
+    elevation: 2,
+  },
+  lowerText: {
+    flexDirection: 'row',
+  },
+  sectionContainer: {
+    backgroundColor: 'white',
+    flex: 1,
+    marginTop: 5,
+  },
+  headerSection: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: '#bdc3c7',
+    height: 20,
+    alignItems: 'center',
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+  inviteButton: {
+    flex: 1,
+    width: 100,
+    height: 30,
+    backgroundColor: '#315F61',
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+    marginLeft: 45,
+  },
+  bsitterName: {
+    fontSize: 13,
+    fontWeight: '400',
+    color: '#315F61',
+  },
+  upperText: {
+    flexDirection: 'row',
+    marginHorizontal: 5,
+    marginLeft: 10,
+    flex: 1,
+    alignItems: 'center',
+  },
+  sitterImage: {
+    width: '100%',
+    borderRadius: 15,
+    resizeMode: 'contain',
+    marginLeft: 45,
+  },
+  bsitterItem: {
+    flexDirection: 'row',
+  },
+  detailPictureContainer: {
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  listBabySitterButton: {
+    marginVertical: 5,
+    width: '100%',
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    marginTop: 20,
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  detailContainer: {
+    marginTop: 15,
+  },
+  name: {
+    alignItems: 'center',
+  },
+  submitButton: {
+    width: 250,
+    height: 30,
+    padding: 5,
+    borderRadius: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  answerButton: {
+    width: 150,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: -15,
+    marginHorizontal: 10,
+    backgroundColor: '#315F61',
+    borderRadius: 10,
+  },
+  headerTitle: {
+    fontSize: 14,
+    color: '#315F61',
+    marginBottom: 5,
+    fontWeight: '800',
+    marginLeft: 5,
+  },
+  optionsText: {
+    fontSize: 15,
+    color: 'gray',
+    fontWeight: 'bold',
+  },
+  profileImg: {
+    width: 70,
+    height: 70,
+    borderRadius: 140 / 2,
+    overflow: 'hidden',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  textAndDayContainer: {
+    flexDirection: 'row',
+  },
+  informationText: {
+    fontSize: 13,
+    marginTop: 15,
+    flexDirection: 'row',
+    color: '#bdc3c7',
+    // backgroundColor: 'red',
+  },
+  contentInformation: {
+    fontSize: 12,
+    paddingLeft: 10,
+    color: '#315F61',
+  },
+  contentInformationDate: {
+    fontSize: 12,
+    paddingLeft: 10,
+    color: '#315F61',
+    fontWeight: '700',
+  },
+  priceText: {
+    fontSize: 15,
+    marginLeft: 150,
+    marginTop: 25,
+    flexDirection: 'row',
+  },
+  detailInformationContainer: {
+    flex: 1,
+    marginTop: 5,
+    marginHorizontal: 5,
+  },
+  detailOptionsContainer: {
+    flex: 1,
+    marginTop: 15,
+  },
+  optionText: {
+    fontSize: 15,
+    marginLeft: 45,
+    marginTop: 25,
+    flexDirection: 'row',
+  },
+
+  optionInformation: {
+    fontSize: 13,
+    paddingLeft: 10,
+    fontWeight: '400',
+  },
+  grayOptionInformation: {
+    color: '#bdc3c7',
+    fontSize: 11,
+    paddingLeft: 10,
+    fontWeight: '200',
+    marginTop: 5,
+  },
+  textOption: {
+    marginHorizontal: 5,
+  },
+});
---
+++ src/screens/parent/SittingList.js
@@ -0,0 +1,258 @@
+/* eslint-disable react/no-unused-state */
+import React, { Component } from 'react';
+import { retrieveToken } from 'utils/handleToken';
+import {
+  StyleSheet,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
+
+import { MuliText } from 'components/StyledText';
+import colors from 'assets/Color';
+import moment from 'moment';
+import Api from '../../api/api_helper';
+import {getSitting} from '../../api/sittingRequest.api';
+
+export default class SittingList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requests: null,
+      invitations: null,
+      userId: 0,
+      refreshing: false,
+    };
+  }
+
+  componentWillMount() {
+    this.getDataAccordingToRole();
+  }
+
+  // for user role of Parent - roleId == 2
+  getDataAccordingToRole = async () => {
+    await retrieveToken().then((res) => {
+      const { userId } = res;
+      this.setState({ userId });
+    });
+
+    const requestBody = {
+      userId: this.state.userId,
+      status: 'DONE',
+    };
+    const rp = await getSitting(requestBody).then(res => {
+      this.setState({ requests: res });
+    });
+  };
+
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
+    this.getDataAccordingToRole().then(() => {
+      this.setState({ refreshing: false });
+    });
+  };
+
+  render() {
+    const { requests } = this.state;
+    return (
+      <View style={styles.container}>
+        <View style={{ alignItems: 'center' }}>
+          {requests != '' && requests ? (
+            <ScrollView>
+              {requests.map((request) => (
+                <TouchableOpacity
+                  key={request.id}
+                  style={{
+                    backgroundColor: '#fff',
+                    marginTop: 20,
+                    marginHorizontal: 20,
+                    borderRadius: 20,
+                  }}
+                  onPress={() => this.props.navigation.navigate('SittingDetail', { requestId: request.id })}
+                >
+                  <View style={{ height: 135 }}>
+                    <View
+                      style={{
+                        flex: 0.2,
+                        backgroundColor: '#78ddb6',
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                      }}
+                    />
+                    <View
+                      style={{
+                        flex: 0.8,
+                        width: 350,
+                        height: 150,
+                        flexDirection: 'row',
+                      }}
+                    >
+                      <View style={styles.leftInformation}>
+                        <MuliText>Yêu cầu từ {request.user.nickname}</MuliText>
+                        <MuliText style={styles.date}>
+                          {moment(request.sittingDate).format('DD-MM-YYYY')}
+                        </MuliText>
+                        <MuliText>
+                          {moment
+                            .utc(request.startTime, 'HH:mm')
+                            .format('HH:mm')}{' '}
+                          -
+                          {moment.utc(request.endTime, 'HH:mm').format('HH:mm')}
+                        </MuliText>
+                        <MuliText>{request.sittingAddress}</MuliText>
+                      </View>
+                      <View style={{ alignItems: 'center' }}>
+                        <View style={styles.statusBoxConfirm}>
+                          <MuliText style={{ fontWeight: '100', color: 'red' }}>
+                            {request.status}
+                          </MuliText>
+                        </View>
+                        <MuliText>{request.totalPrice} VND</MuliText>
+                      </View>
+                    </View>
+                  </View>
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+          ) : (
+            <View style={styles.noRequest}>
+              <MuliText style={styles.noRequestText}>
+                Hiện tại bạn không có yêu cầu nào
+              </MuliText>
+              <MuliText>Nhấn để tạo yêu cầu</MuliText>
+              <Image
+                source={require('assets/images/no-request.jpg')}
+                style={styles.noRequestImage}
+              />
+            </View>
+          )}
+        </View>
+      </View>
+    );
+  }
+}
+
+SittingList.navigationOptions = {
+  title: 'Yêu cầu giữ trẻ',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#dfe6e9',
+  },
+  createRequest: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    position: 'absolute',
+    opacity: 0.9,
+    bottom: 10,
+    right: 10,
+  },
+  textHeaderParent: {
+    fontSize: 20,
+    color: '#315f61',
+    fontWeight: 'bold',
+    lineHeight: 20,
+  },
+  textHeaderBsitter: {
+    fontSize: 20,
+    color: '#315f61',
+    fontWeight: 'bold',
+    lineHeight: 20,
+    alignItems: 'flex-start',
+  },
+  statusBoxPending: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'green',
+    width: 90,
+    height: 40,
+    padding: 10,
+  },
+  statusBoxConfirm: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'green',
+    width: 90,
+    height: 40,
+    padding: 10,
+  },
+  noRequest: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    marginTop: 20,
+    paddingHorizontal: 20,
+    paddingTop: 30,
+  },
+  noRequestText: {
+    marginVertical: 10,
+    marginHorizontal: 30,
+    paddingTop: 20,
+    fontSize: 18,
+    color: '#315f61',
+    fontWeight: 'bold',
+  },
+  noRequestImage: {
+    width: 261,
+    height: 236,
+    marginVertical: 20,
+  },
+  requestItem: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    height: 120,
+    marginHorizontal: 30,
+    alignItems: 'center',
+    marginTop: 20,
+    borderRadius: 15,
+  },
+  leftInformation: {
+    // backgroundColor: 'blue',
+    margin: 15,
+    paddingHorizontal: 5,
+    flex: 1,
+  },
+  rightInformation: {
+    // backgroundColor: 'green',
+    flex: 1,
+    alignItems: 'flex-end',
+    paddingRight: 20,
+  },
+  headerTitle: {
+    marginHorizontal: 15,
+    marginTop: 30,
+    fontSize: 25,
+    color: '#315F61',
+    marginBottom: 10,
+    fontWeight: '800',
+  },
+  scheduleContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+    paddingTop: 20,
+    marginBottom: 20,
+    flex: 0.1,
+    backgroundColor: '#fff',
+  },
+  scheduleContainerBsitter: {
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginTop: 30,
+    paddingTop: 20,
+    paddingLeft: 30,
+    flex: 0.25,
+    backgroundColor: '#fff',
+  },
+  date: {
+    marginBottom: 10,
+    color: colors.darkGreenTitle,
+    fontWeight: '400',
+    fontSize: 15,
+  },
+});
