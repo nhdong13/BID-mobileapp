1e3475c5992773a613f0dd0ece2a27b0f64ea8fa
Phuc Phung <phucphung@phungthienphuc18@gmail.com>
2019-11-18 14:50:46 +0700

    work on invitations, 3 different types, upcoming - peding - waiting

--- src/screens/babysitter/WaitingInvitation.js
+++ src/screens/babysitter/WaitingInvitation.js
@@ -6,7 +6,7 @@ import moment from 'moment';
 import colors from 'assets/Color';
 import { formater } from 'utils/MoneyFormater';
 
-class UpcomingSitting extends Component {
+class WaitingInvitation extends Component {
   constructor(props) {
     super(props);
     this.state = {};
@@ -15,10 +15,16 @@ class UpcomingSitting extends Component {
   render() {
     const { invitation, navigation } = this.props;
     return (
-      <View style={styles.container}>
-        {invitation.status !== 'PENDING' && (
+      <View>
+        {invitation.status === 'ACCEPTED' && (
           <TouchableOpacity
             key={invitation.id}
+            style={{
+              backgroundColor: '#fff',
+              marginTop: 20,
+              marginHorizontal: 20,
+              borderRadius: 20,
+            }}
             onPress={() =>
               invitation.status == 'EXPIRED' || invitation.status == 'DENIED'
                 ? null
@@ -29,15 +35,15 @@ class UpcomingSitting extends Component {
           >
             <View style={styles.requestItemSitter}>
               <View style={styles.leftInformationSitter}>
-                <MuliText>
-                  Lời mời từ {invitation.sittingRequest.user.nickname}
-                </MuliText>
                 <MuliText style={styles.date}>
                   {moment(invitation.sittingRequest.sittingDate).format(
                     'dddd Do MMMM',
                   )}
                 </MuliText>
-                <MuliText style={{ marginTop: 5, color: '#7edeb9' }}>
+                <MuliText>
+                  Lời mời từ {invitation.sittingRequest.user.nickname}
+                </MuliText>
+                <MuliText style={{ marginTop: 10, color: '#7edeb9' }}>
                   {moment
                     .utc(invitation.sittingRequest.startTime, 'HH:mm')
                     .format('HH:mm')}{' '}
@@ -47,16 +53,21 @@ class UpcomingSitting extends Component {
                     .format('HH:mm')}
                 </MuliText>
               </View>
+              {/* OVERLAP
+          ACCEPTED
+          EXPIRED
+          CANCELED
+          DONE */}
               <View style={styles.rightInformation}>
                 <View style={styles.statusBoxPending}>
                   <MuliText
-                    style={{ fontWeight: '100', color: colors.pending }}
+                    style={{ fontWeight: '100', color: colors.lightGreen }}
                   >
-                    Đang chờ {invitation.status}
+                    Đang chờ phụ huynh
                   </MuliText>
                 </View>
                 <MuliText>
-                  gia tien {formater(invitation.sittingRequest.totalPrice)}VND
+                  {formater(invitation.sittingRequest.totalPrice)}VND
                 </MuliText>
               </View>
             </View>
@@ -67,14 +78,12 @@ class UpcomingSitting extends Component {
   }
 }
 
-export default withNavigation(UpcomingSitting);
+export default withNavigation(WaitingInvitation);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    // backgroundColor: 'red',
-    backgroundColor: '#dfe6e9',
+    backgroundColor: '#fff',
   },
   containerBsitter: {
     flex: 1,
@@ -107,7 +116,14 @@ const styles = StyleSheet.create({
     // backgroundColor: 'green',
     height: 40,
   },
-
+  statusBoxConfirm: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'green',
+    width: 100,
+    height: 50,
+    padding: 5,
+  },
   noRequest: {
     flex: 1,
     backgroundColor: 'white',
@@ -130,21 +146,20 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
   requestItemSitter: {
-    justifyContent: 'space-between',
     flex: 1,
     flexDirection: 'row',
     backgroundColor: 'white',
     height: 150,
     width: 350,
-    margin: 10,
+    marginHorizontal: 15,
     alignItems: 'center',
     borderRadius: 15,
-    // marginBottom: 5,
+    marginBottom: 5,
   },
   requestItem: {
     flex: 1,
     flexDirection: 'row',
-    backgroundColor: 'violet',
+    backgroundColor: 'white',
     height: 180,
     marginHorizontal: 30,
     alignItems: 'center',
@@ -165,7 +180,7 @@ const styles = StyleSheet.create({
   rightInformation: {
     flex: 1,
     alignItems: 'flex-end',
-    paddingRight: 10,
+    marginRight: 10,
   },
   scheduleContainer: {
     alignItems: 'center',
@@ -186,8 +201,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   date: {
-    marginTop: 10,
-    // marginBottom: 10,
+    marginTop: 5,
+    marginBottom: 10,
     color: colors.darkGreenTitle,
     fontWeight: '400',
     fontSize: 15,
